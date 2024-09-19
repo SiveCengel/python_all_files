8fe3985e36b79ec9ed32a07082c74505b3d89245
SiveCengel <119365372+SiveCengel@users.noreply.github.com>
2024-09-19 13:53:33 +0300

    Add files via upload

---
+++ fonkdegergon.py
@@ -0,0 +1,33 @@
+def hello():
+    return "Merhaba"
+
+
+
+info=hello()
+print(info)
+
+#?örn: 
+
+# def calc():
+#     2*2
+#     return 100
+
+# calc_result=calc()
+# print(calc_result)
+
+
+# def greetings(name):
+#     print(f"{hello()} {name}")
+
+# greetings("Django")
+
+# def greetings_v2(first_name,last_name):
+#     return [first_name,last_name]
+
+# print(greetings_v2("Python","Django"))
+
+# def greetings_v3(product,platform="Django"):
+#     print(product,platform)
+
+# greetings_v3("Dentorium")
+# greetings_v3("Doktorium","Flask")
