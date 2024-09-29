259b3df4d9f358b40582b74611ce5bf98db77f91
SiveCengel <119365372+SiveCengel@users.noreply.github.com>
2024-09-29 14:58:07 +0300

    Add files via upload

---
+++ tupleilecalisma.py
@@ -0,0 +1,29 @@
+# tuple nedir ?  değiştirilemeyen bilgiler...
+# tuple nasil tanimlanir ? 
+# list ile farki nedir
+# len kullanimi
+# tuple'a veri nasil eklenir/eklenebilir mi ?   hayır!
+# tuple'dan veri nasil silinir/silinebilir mi ? ancak içerisindeki bilgiler bir dict veya array halinde ise 
+
+
+SERVER_INFO = ("user12w4353", "fidsfoierofihersoifgherih")
+
+
+print(SERVER_INFO)
+print(len(SERVER_INFO))
+
+for item in SERVER_INFO:
+    print(item)
+
+
+user = ("Derya", "Django", [1, 2, 3,], {"age": 18, "gender": "f"})
+
+user[2].append(100)
+user[3]["lang"] = "Python"
+
+del(user[3]["gender"])
+
+print(user[2])
+print(user[3])
+
+# del(user[1])
